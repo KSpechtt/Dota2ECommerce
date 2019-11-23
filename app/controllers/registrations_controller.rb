89a2ab2b2b93dd072d8f103016010daf3f89faff
Kyle Specht <habitatt11@gmail.com>
2019-11-22 18:40:14 -0600

    Rubocop Touch Ups

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,29 @@
 # frozen_string_literal: true
 
+#  Class
 class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:full_name, :email, :city, :address, :province_id, :password, :password_confirmation)
+    params.require(:user)
+          .permit(:full_name,
+                  :email,
+                  :city,
+                  :address,
+                  :province_id,
+                  :password,
+                  :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:full_name, :email, :city, :address, :province_id, :password, :password_confirmation, :current_password)
+    params.require(:user)
+          .permit(:full_name,
+                  :email,
+                  :city,
+                  :address,
+                  :province_id,
+                  :password,
+                  :password_confirmation,
+                  :current_password)
   end
 end
